f5dd388f88e0f95907b1934b91179cb63ead10fa
Dragoninabackpack <dragoninabackpack@gmail.com>
2024-11-26 17:51:06 -0500

    this is my commit for today

---
+++ thing.py
@@ -0,0 +1,14 @@
+
+      if computer_input == 7:
+        print('stop making a sandwich! things arent right!')
+        print("1:stop talking")
+        print("2:SHUt UP")
+        print("3:i know")
+        if user_input == "1":
+          print('you cant make me, im you')
+          section2()
+        if user_input == "2":
+          print("no")
+          section2()
+        if user_input == "3":
+          print('then stop and listen')
